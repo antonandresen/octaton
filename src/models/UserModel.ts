1b1713cc62eb3fa297dd74ebd2585f56df6e0ef7
Anton <anton_real_madrid@icloud.com>
2020-06-11 23:52:12 +0200

    Added UserModel schema

---
+++ src/models/UserModel.ts
@@ -0,0 +1,20 @@
+import mongoose, { Document } from 'mongoose';
+
+import { IUser } from './types';
+
+type DBUser = IUser & Document;
+
+const UserSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: [true, 'Email is required'],
+    maxlength: [80, 'Email can not be longer than 80 characters'],
+  },
+  twitchID: {
+    type: String,
+    required: [true, 'Twitch ID is required'],
+  },
+});
+
+const User = mongoose.model<DBUser>('User', UserSchema);
+export default User;
--- src/models/types.ts
+++ src/models/types.ts
@@ -1,3 +1,4 @@
-export interface User {
+export interface IUser {
+  email: string;
   twitchID: string;
 }
